7fb745b04ae9d660b3227e8512fe339fa5ebb77f
pasampat <priyasampat.college@gmail.com>
2025-07-13 13:24:32 -0400

    Set up complete

---
+++ .history/main_20250713130408.py
@@ -0,0 +1,9 @@
+# main.py
+import os
+import re
+import string
+import collections
+import csv
+from PyPDF2 import PdfReader  # Install with: pip install PyPDF2
+
+S
---
+++ .history/main_20250713131433.py
@@ -0,0 +1,11 @@
+# main.py
+import os
+import re
+import string
+import collections
+import csv
+from PyPDF2 import PdfReader  # Install with: pip install PyPDF2
+
+from stopwords import STOP_WORDS
+
+print("Setup complete!")
