00a829699c092ea9003828e7d1bf51997ef42d08
pasampat <priyasampat.college@gmail.com>
2025-08-04 17:47:56 -0400

    Remove .history folder from repo and ignore it

--- .history/main_20250713130306.py
+++
@@ -1,7 +0,0 @@
-# main.py
-import os
-import re
-import string
-import collections
-import csv
-from PyPDF2 import PdfReader  # Install with: pip install PyPDF2
--- .history/stopwords_20250713130403.py
+++
@@ -1,8 +0,0 @@
-STOP_WORDS = [
-    'the', 'and', 'is', 'in', 'to', 'of', 'for', 'with', 'on', 'a', 'an',
-    'by', 'at', 'as', 'it', 'this', 'that', 'from', 'or', 'be', 'are',
-    'was', 'were', 'has', 'have', 'had', 'but', 'not', 'your', 'you', 'our',
-    'we', 'they', 'he', 'she', 'him', 'her', 'his', 'their', 'will', 'can',
-    'if', 'about', 'which', 'more', 'also', 'so', 'than', 'other', 'when'
-    # ...expand as needed
-]
